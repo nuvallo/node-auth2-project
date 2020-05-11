d3d54e30f29ae590abff4cf94819414b4aaf067d
nuvallo <nuvallo.inc@gmail.com>
2020-05-11 17:05:35 -0400

    basic server up

---
+++ index.js
@@ -0,0 +1,8 @@
+const server = require("./api/server");
+
+const hostName = process.env.hostName || "127.0.0.1";
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on ${hostName}:${PORT}`);
+});
